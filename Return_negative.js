32b5b9053e7d2de506ed959a9c3c9bef74ea433a
iarunabho <arunabho95@gmail.com>
2018-10-01 22:26:26 +0530

    Added Another simple function

--- Return_negative.js
+++ Return_negative.js
@@ -2,3 +2,9 @@
 function makeNegative(num) {
     return Math.sign(num)<0? num:-Math.abs(num);
   }
+
+/* 2nd way of doing #Simple maths
+function makeNegative(num) {
+  return num <= 0 ? num : num*-1;
+}
+*/
